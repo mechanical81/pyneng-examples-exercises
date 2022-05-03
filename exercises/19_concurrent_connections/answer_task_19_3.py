1b15ef33576e184418917cc5b93fade61faa2b02
python <python@debian>
2022-05-03 12:42:32 -0500

    Add Exercise 19_3

--- exercises/19_concurrent_connections/answer_task_19_3.py
+++ exercises/19_concurrent_connections/answer_task_19_3.py
@@ -39,42 +39,41 @@ router ospf 1
 
 Проверить работу функции на устройствах из файла devices.yaml и словаре commands
 """
+from itertools import repeat
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
-# Этот словарь нужен только для проверки работа кода, в нем можно менять IP-адреса
-# тест берет адреса из файла devices.yaml
-commands = {
-    "192.168.100.3": "sh run | s ^router ospf",
-    "192.168.100.1": "sh ip int br",
-    "192.168.100.2": "sh int desc",
-}
-
+from netmiko import ConnectHandler, NetMikoTimeoutException
 import yaml
-from netmiko import ConnectHandler
-from concurrent.futures import ThreadPoolExecutor
+
+
+commands = {
+    "192.168.100.1": "sh ip int br",
+    "192.168.100.2": "sh arp",
+    "192.168.100.3": "sh ip int br",
+}
 
 
 def send_show_command(device, command):
     with ConnectHandler(**device) as ssh:
         ssh.enable()
-        output = ssh.send_command(command)
-        output = ssh.find_prompt() + command + "\n" + output + "\n"
-        return output
-    
+        result = ssh.send_command(command)
+        prompt = ssh.find_prompt()
+    return f"{prompt}{command}\n{result}\n"
+
 
 def send_command_to_devices(devices, commands_dict, filename, limit=3):
-    future_list = []
-    with ThreadPoolExecutor(max_workers = limit) as executor, open(filename, 'w') as outfile:
-        for device in devices:
-            command = commands_dict[device['host']]
-            future = executor.submit(send_show_command, device, command)
-            future_list.append(future)
-        
-        for future in future_list:
-            outfile.write(future.result())
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        futures = [
+            executor.submit(send_show_command, device, commands_dict[device["host"]])
+            for device in devices
+        ]
+        with open(filename, "w") as f:
+            for future in as_completed(futures):
+                f.write(future.result())
 
 
-if __name__ == '__main__':
-    with open('devices.yaml') as f:
-        devices = yaml.safe_load(f)
-    
-    send_command_to_devices(devices, commands, 'out.txt') 
+if __name__ == "__main__":
+    command = "sh ip int br"
+    with open("devices.yaml") as f:
+        devices = yaml.load(f)
+    send_command_to_devices(devices, commands, "result.txt")
