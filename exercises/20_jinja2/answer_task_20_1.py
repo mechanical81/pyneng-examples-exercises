eeb2b7ec739dbd48873b8ee4826547d436f3de28
python <python@debian>
2022-05-04 12:50:39 -0500

    Add Exercise 20_1

--- exercises/20_jinja2/answer_task_20_1.py
+++ exercises/20_jinja2/answer_task_20_1.py
@@ -13,25 +13,21 @@
 Проверить работу функции на шаблоне templates/for.txt
 и данных из файла data_files/for.yml.
 
-Важный нюанс: надо получить каталог из параметра template и использовать его, нельзя
-указывать текущий каталог в FileSystemLoader - то есть НЕ надо делать так FileSystemLoader(".").
-Указание текущего каталога, сломает работу других заданий/тестов.
-
 """
-import yaml
 import os
 from jinja2 import Environment, FileSystemLoader
+import yaml
 
 
 def generate_config(template, data_dict):
-    template_dir, template_file = os.path.split(template)
-    env = Environment(loader = FileSystemLoader(template_dir))
-    tmpl = env.get_template(template_file)
-    tmpl_out = tmpl.render(data_dict)
-    return tmpl_out
+    templ_dir, templ_file = os.path.split(template)
+    env = Environment(
+        loader=FileSystemLoader(templ_dir), trim_blocks=True, lstrip_blocks=True
+    )
+    templ = env.get_template(templ_file)
+    return templ.render(data_dict)
 
 
-# так должен выглядеть вызов функции
 if __name__ == "__main__":
     data_file = "data_files/for.yml"
     template_file = "templates/for.txt"
